e58c98233213355332d434542878cfa500436e47
Maged <mmothana@cps.edu>
2024-04-23 10:25:07 -0500

    first change

--- a8.py
+++ a8.py
@@ -1,7 +1,42 @@
 from neural import *
 
-print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+# print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+# xor_data = [
+#     ([0,0], [0]),
+#     ([1,0], [1]),
+#     ([0,1], [1]),
+#     ([1,1], [0])
+# ]
 
+# xor_nn = NeuralNet(2, 2, 1)
+# xor_nn.train(xor_data, iters=1000, print_interval=100)
 
+# print(xor_nn.test_with_expected(xor_data))
+# print(xor_nn.evualate([1,1]))
 
+print("<<<<<<<<<<<<<<<<<< Voter Data >>>>>>>>>>>>>>>")
+
+voter_data = [
+    ([.9, .6, .8, .3, .1], [1]),
+    ([.8, .8, .4, .6, .4], [1]),
+    ([.7, .2, .4, .6, .3], [1]),
+    ([.5, .5, .8, .4, .8], [0]),
+    ([.3, .1, .6, .8, .8], [0]),
+    [(.6, .3, .4, .3, .6), [0]]
+]
+
+voter_nn = NeuralNet(5, 6, 1)
+voter_nn.train(voter_data)
+
+print(voter_nn.test_with_expected(voter_data))
+
+print(voter_nn.test([
+    [1, 1, 1, .1, .1],
+    [.5, .2, .1, .7, .7],
+    [.8, .3, .3, .3, .8]
+    [.8, .3, .3, .8, .3]
+    [.9, .8, .8, .3, .6]
+]))
+
+ptiny(voter_nn.evaluate)
